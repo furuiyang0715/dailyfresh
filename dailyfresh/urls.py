68f1dfd52ff26073617be8225684bcbc1fe60da1
furuiyang <furuiyang0715@gmail.com>
2020-08-08 17:38:07 +0800

    little adjustment.

--- dailyfresh/urls.py
+++ dailyfresh/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r"^user/", include("apps.user.urls")),
+    path(r"^order/", include("apps.user.urls")),
+    path(r"^cart/", include("apps.user.urls")),
+    path(r"^goods/", include("apps.user.urls")),
 ]
