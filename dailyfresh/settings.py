6d2580f6cb1e22e580386cf7d949f2932c95f1ec
furuiyang <furuiyang0715@gmail.com>
2020-08-08 17:46:10 +0800

    配置模板文件路径

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -42,10 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 注册 my apps
-    'user',
-    'goods',
-    'cart',
-    'order',
+    'user',   # 用户模块
+    'goods',  # 商品模块
+    'cart',   # 购物车模块
+    'order',  # 订单模块
 ]
 
 MIDDLEWARE = [
@@ -62,13 +62,13 @@ ROOT_URLCONF = 'dailyfresh.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.templates.context_processors.debug',
+                'django.templates.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
