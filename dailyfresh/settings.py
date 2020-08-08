1e39b0ad7b80538dde398891eed8343c16c35f96
furuiyang <furuiyang0715@gmail.com>
2020-08-08 17:23:13 +0800

    注册 my apps

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 注册 my apps
+    'apps.user',
+    'apps.goods',
+    'apps.cart',
+    'apps.order',
 ]
 
 MIDDLEWARE = [
