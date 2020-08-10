88c1131647725c93e9c536ffc4f07c4d0b7ed584
furuiyang <furuiyang0715@gmail.com>
2020-08-10 15:21:23 +0800

    初步的注册页面响应

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,5 +4,6 @@ from apps.user import views
 
 urlpatterns = [
     url(r"^register/$", views.register, name='register'),
+    url(r"^register_handle/$", views.register_handle, name='register_handle'),
 
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,3 +6,16 @@ from django.shortcuts import render
 
 def register(request):
     return render(request, "register.html")
+
+
+def register_handle(request):
+    # 接收数据
+
+    # 校验数据
+
+    # 进行业务逻辑处理
+
+    # 返回应答
+
+    #
+    return HttpResponse("注册成功")
