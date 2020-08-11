81e177ea0d69cd17bb43bdd77d93f3200fd36ea4
furuiyang <furuiyang0715@gmail.com>
2020-08-11 11:07:16 +0800

    说明解密的时输入 bytes 或者 str 均可

--- temp/demo_2.py
+++ temp/demo_2.py
@@ -6,9 +6,14 @@ info = {"confirm": 1}
 # 加密
 en_info = ser.dumps(info)
 print(en_info)
+en_info2 = en_info.decode()
+print(en_info2)
 # 解密
+# 说明解密的时候 输入 bytes 以及 str 均可
 de_info = ser.loads(en_info)
 print(de_info)
+de_info2 = ser.loads(en_info2)
+print(de_info2)
 
 
 # from django.conf import settings
