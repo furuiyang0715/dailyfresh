df64fa2fa14fd8fc66b1d03410f29bd134a6ab96
furuiyang <furuiyang0715@gmail.com>
2020-08-12 11:27:14 +0800

    Little

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 # from apps.user import views
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 
 from apps.user.views import RegisterView, ActiveView, LoginView, UserInfoView, UserOrderView, AddressView
 
@@ -16,9 +16,13 @@ urlpatterns = [
     url(r"^login/$", LoginView.as_view(), name='login'),    # 用户登录
 
     # login_required 要在视图函数上使用 as_view() 的返回值是函数 所以我们要在这里手动调用一次
-    url(r"^$", login_required(UserInfoView.as_view()), name='info'),    # info
-    url(r"^order/$", login_required(UserOrderView.as_view()), name='order'),    # order
-    url(r"^site/$", login_required(AddressView.as_view()), name='site'),    # site
+    # url(r"^$", login_required(UserInfoView.as_view()), name='info'),    # info
+    # url(r"^order/$", login_required(UserOrderView.as_view()), name='order'),    # order
+    # url(r"^site/$", login_required(AddressView.as_view()), name='site'),    # site
+
+    url(r"^$", UserInfoView.as_view(), name='info'),  # info
+    url(r"^order/$", UserOrderView.as_view(), name='order'),  # order
+    url(r"^site/$", AddressView.as_view(), name='site'),  # site
 
 
 ]
