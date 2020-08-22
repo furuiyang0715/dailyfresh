c86e1f24fe9f42d9e7e7e6db3d2ef891b00c05e9
furuiyang <furuiyang0715@gmail.com>
2020-08-22 15:09:57 +0800

    将首页的函数修改为类视图

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
 
 
-def index(request):
-    return render(request, "index.html")
+# def index(request):
+#     return render(request, "index.html")
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, "index.html")
