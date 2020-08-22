58992ad338230d6abd9096b7a28d5308032c7160
furuiyang <furuiyang0715@gmail.com>
2020-08-22 15:34:21 +0800

    首页信息的后端查询

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from django.views import View
 
+from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+
 
 class IndexView(View):
     """首页"""
     def get(self, request):
         """显示首页"""
-        return render(request, "index.html")
+
+        # (1) 获取商品的种类信息
+        types = GoodsType.objects.all()
+
+        # (2) 获取轮播商品的信息
+        # 需要根据展示的顺序进行排序
+        # "index": 默认按照升序排列
+        # - "index": 安装降序排列
+        goods_banners = IndexGoodsBanner.objects.all().order_by("index")
+
+        # (3) 获取首页促销活动信息
+        goods_promotion = IndexPromotionBanner.objects.all().order_by("index")
+
+        # (4) 获取首页分类商品展示信息
+        goods_type = IndexTypeGoodsBanner.objects.all()
+
+        # (5) 获取用户购物车中商品的数目
+        cart_count = 0
+
+        # 组织模板上下文
+        context = {
+            "types": types,
+            "goods_banners": goods_banners,
+            "goods_promotion": goods_promotion,
+            "goods_type": goods_type,
+            "cart_count": cart_count,
+        }
+
+        return render(request, "index.html", context)
